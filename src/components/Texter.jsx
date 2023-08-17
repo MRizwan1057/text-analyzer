2482544a104dd8899c4f97ef9819d9265d74f1cb
Muhammad Rizwan <96943208+MRizwan1057@users.noreply.github.com>
2023-08-17 22:26:24 +0500

    more functions added

--- src/components/Texter.jsx
+++ src/components/Texter.jsx
@@ -4,6 +4,7 @@ const Texter = () => {
   const [text, setText] = useState("");
   const validateText = () => {
     if (text.length === 0) {
+      // setText("Please enter some text Here first....");
       alert("Please enter some text in the box....");
     }
   };
@@ -23,16 +24,32 @@ const Texter = () => {
   };
   const cpHandler = () => {
     validateText();
-    const words = text.split(" ");
+    // const words = text.split("");
+    const words = text.split(/[ ,.]+/);
     const capitalized = words.map(
       (word) => word.charAt(0).toUpperCase() + word.slice(1)
     );
-    return setText(capitalized.join(" "));
+    setText(capitalized.join(" "));
   };
   const clHandler = () => {
     validateText();
     setText("");
   };
+  const exspHandler = () => {
+    validateText();
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  const copHandler = async () => {
+    validateText();
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Content copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+
   return (
     <>
       <div className="container my-5">
@@ -51,31 +68,45 @@ const Texter = () => {
               ></textarea>
               <button
                 type="button"
-                className="btn btn-primary m-2"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
                 onClick={upHandler}
               >
                 Upper Case
               </button>
               <button
                 type="button"
-                className="btn btn-primary m-2"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
                 onClick={loHandler}
               >
                 Lower Case
               </button>
               <button
                 type="button"
-                className="btn btn-primary m-2"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
                 onClick={cpHandler}
               >
                 Capitalize
               </button>
               <button
                 type="button"
-                className="btn btn-primary m-2"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
                 onClick={clHandler}
               >
                 Clear
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
+                onClick={copHandler}
+              >
+                Copyto clipboard
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary m-2 fw-bold shadow-sm"
+                onClick={exspHandler}
+              >
+                Remove Spaces
               </button>
             </form>
           </div>
@@ -95,10 +126,12 @@ const Texter = () => {
               <div className="sumr_items">
                 <h4>Preview</h4>
                 <p className="p-3 border border-2 border-dark rounded preview">
-                  {text}
+                  {text.length > 0
+                    ? text
+                    : "Enter some text in the box above to preview here..."}
                 </p>
               </div>
-              <p className="py-2">
+              <p className="pt-2">
                 Designed & Developed by |<b> M.Rizwan</b>
               </p>
             </div>
